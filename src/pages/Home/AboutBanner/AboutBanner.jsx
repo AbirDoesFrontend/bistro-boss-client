60fd5adcf70214fe437737dae0d25924a0d9a7a0
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-05-26 01:21:58 +0600

    About Banner

---
+++ src/pages/Home/AboutBanner/AboutBanner.jsx
@@ -0,0 +1,14 @@
+import './AboutBanner.css'
+
+const AboutBanner = () => {
+    return (
+        <div className="max-w-screen-xl about-banner mt-20 mb-20">
+            <div className='about-banner-content text-[#151515] text-center'>
+                <h2 className='text-4xl uppercase'>Bistro Boss</h2>
+                <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutBanner;
