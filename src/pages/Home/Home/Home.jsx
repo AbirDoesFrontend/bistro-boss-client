f0ab1b2e89613d9f4ebaa71276cff82ebe10d67c
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-05-28 02:25:28 +0600

    React Helmet Dynamic Title

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,10 +4,14 @@ import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import Testimonial from "../Testimonial/Testimonial";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner />
             <Category />
             <AboutBanner />
