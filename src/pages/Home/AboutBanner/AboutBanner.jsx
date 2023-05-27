201839d7eac560be266dc0d7e94b4da68008fea0
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-05-28 02:48:41 +0600

    About Banner Parallax Effect

--- src/pages/Home/AboutBanner/AboutBanner.jsx
+++ src/pages/Home/AboutBanner/AboutBanner.jsx
@@ -2,7 +2,7 @@ import './AboutBanner.css'
 
 const AboutBanner = () => {
     return (
-        <div className="max-w-screen-xl about-banner mt-20 mb-20">
+        <div className="max-w-screen-xl about-banner mt-20 mb-20 bg-fixed">
             <div className='about-banner-content text-[#151515] text-center'>
                 <h2 className='text-4xl uppercase'>Bistro Boss</h2>
                 <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
