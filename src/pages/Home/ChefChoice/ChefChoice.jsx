73c75548256913b594287fad7ffdf65ee5950b8b
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-05-29 12:47:06 +0600

    Server side initialized

--- src/pages/Home/ChefChoice/ChefChoice.jsx
+++ src/pages/Home/ChefChoice/ChefChoice.jsx
@@ -7,7 +7,7 @@ const ChefChoice = () => {
     const [menus, setMenus] = useState([])
 
     useEffect(() => {
-        fetch('menu.json')
+        fetch('http://localhost:5000/menu')
         .then(res => res.json())
         .then(data => setMenus(data))
     }, [])
