17255f59c7b816350fe4d19e7db8ff86eff65d8b
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-04 11:58:59 +0600

    Manage items admin page done

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,11 +1,86 @@
+import { FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-    return (
-        <div className="w-full">
-            <SectionTitle heading={'manage all items'} subHeading={'hurry up'}/>
-        </div>
-    );
+  const [menu , , refetch] = useMenu();
+  const [axiosSecure] = useAxiosSecure();
+
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/menu/${item._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            refetch()
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="w-full">
+      <SectionTitle heading={"manage all items"} subHeading={"hurry up"} />
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th className="inter">#</th>
+              <th className="inter">Item Image</th>
+              <th className="inter">Item Name</th>
+              <th className="inter">Price</th>
+              <th className="inter">Update</th>
+              <th className="inter">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {menu.map((item, index) => (
+              <tr key={item._id}>
+                <th className="inter">{index + 1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td className="inter">{item.name}</td>
+                <td className="inter">${item.price}</td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">Coming Soon</button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-lg btn-error text-white"
+                    onClick={() => handleDelete(item)}
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageItems;
